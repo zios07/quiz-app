cfdb23ae12294a06ff7fe2d374b0b128a3e0a891
zios07 <zakaria.kaoukab@gmail.com>
2019-06-23 00:16:32 +0100

    backend ready

--- common-ui/src/app/components/common/register/register.component.ts
+++ common-ui/src/app/components/common/register/register.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { delay } from 'rxjs/operators';
 import { Account } from 'src/app/models/Account';
-import { Role } from 'src/app/models/Role';
 import { User } from 'src/app/models/User';
 import { AuthenticationService } from 'src/app/services/authentication.service';
-import { TokenService } from 'src/app/services/token.service';
-import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-register',
@@ -25,9 +23,7 @@ export class RegisterComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private route: ActivatedRoute,
     private formBuilder: FormBuilder,
-    private tokenService: TokenService,
     private authService: AuthenticationService,
     private toastr: ToastrService
   ) {
@@ -39,7 +35,6 @@ export class RegisterComponent implements OnInit {
       password: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(32)])],
       firstName: ['', Validators.compose([Validators.required])],
       lastName: ['', Validators.compose([Validators.required])],
-      email: ['', Validators.compose([Validators.required])],
       bDate: ['', Validators.compose([Validators.required])]
     });
   }
@@ -48,7 +43,6 @@ export class RegisterComponent implements OnInit {
     this.submitted = true;
     const user = this.form.value;
     user.account = new Account(this.form.value.username, this.form.value.password);
-    user.role = new Role(this.form.value.role);
     this.authService.register(user).pipe(delay(1500)).subscribe(resp => {
       this.authService.setConnectedUser(resp);
       this.router.navigate(['/']);
