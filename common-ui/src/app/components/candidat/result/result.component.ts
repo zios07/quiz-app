3ce449f8a686e45592e5982749072b41e99e5b51
zios07 <zakaria.kaoukab@gmail.com>
2019-06-29 02:38:20 +0100

    result component

--- common-ui/src/app/components/candidat/result/result.component.ts
+++ common-ui/src/app/components/candidat/result/result.component.ts
@@ -7,6 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultComponent implements OnInit {
 
+
+  public chartType: string = 'doughnut';
+
+  public chartDatasets: Array<any> = [
+    { data: [800, 50, 20], label: 'My First dataset' }
+  ];
+
+  public chartLabels: Array<any> = ['Correct answers', 'Wrong answers', 'Non answered'];
+
+  public chartColors: Array<any> = [
+    {
+      backgroundColor: ['green', 'red', 'gray'],
+      hoverBackgroundColor: ['#3CB371', '#F08080', '#D3D3D3'],
+      borderWidth: 2,
+    }
+  ];
+
+
+  public chartOptions: any = {
+    responsive: true
+  };
+  public chartClicked(e: any): void { }
+  public chartHovered(e: any): void { }
+
   constructor() { }
 
   ngOnInit() {
